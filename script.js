cfbb172496da2b70004bb98bde233f925ce1b21d
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2024-08-25 01:53:15 +0000

    Download Button added..

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-let inputText = document.getElementById('inputText');
-let imgBox = document.getElementById('imgBox');
-let QRImg = document.getElementById('QRImg');
+const imgBox = document.getElementById("imgBox");
+const QRImg = document.getElementById('QRImg');
+const input = document.getElementById('inputText');
+const downloadBtn = document.getElementById('downloadBtn');
 
 function generateQR(){
-    if(inputText.value.length > 0){
-        QRImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + inputText.value;
-        imgBox.classList.add('show-img');
+        if(input.value.length > 0){
+            const qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(input.value);
+            QRImg.src = qrCodeUrl;
+            imgBox.classList.add("show-img")
+
+        // Download Option for QR Code
+        QRImg.onload = function(){
+            downloadBtn.onclick = function(){
+                fetch(qrCodeUrl)
+                    .then(res => res.blob())
+                    .then(blob => {
+                        const url = window.URL.createObjectURL(blob);
+                        const link = document.createElement('a');
+                        link.href = url;
+                        link.download = `${input.value || 'QRCode'}.png`;
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                        window.URL.revokeObjectURL(url);
+                    })
+                    .catch(() => {
+                        alert("Failed to download");
+                    })
+            }
+        }
     }
     else{
-        inputText.classList.add('error');
+        input.classList.add('error');
         setTimeout(function(){
-            inputText.classList.remove('error');
+            input.classList.remove('error');
         }, 1000)
     }
 }
